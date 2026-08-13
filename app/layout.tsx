75efcb4acef5850e95c2cba56e2d87a7721fd6e5
ajadversalo <ajadvers@gmail.com>
2026-08-12 23:42:50 -0700

    Redesign AJ Hub in dark graphite

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const viewport: Viewport = {
-  themeColor: "#161616",
+  themeColor: "#0d0f12",
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
