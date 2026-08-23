fc5593a57be16a0606e3ee285fe2a2a3190b897e
ajadversalo <ajadvers@gmail.com>
2026-08-22 23:27:53 -0700

    new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     images: ["/og.png"],
   },
   icons: {
-    icon: [{ url: "/favicon.svg", type: "image/svg+xml" }, { url: "/icon-192.png", sizes: "192x192", type: "image/png" }],
+    icon: [{ url: "/favicon-aj.png", type: "image/png" }],
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
   },
   other: { "ajhub-app": "1" },
